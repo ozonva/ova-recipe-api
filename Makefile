d425bb60beeb15e82e3baaf70b36f4760001a1bd
vasily.popov <vasily.popov@arrival.com>
2021-08-15 17:57:28 +0300

    [task-3] Add Recipe entity

--- Makefile
+++ Makefile
@@ -5,4 +5,6 @@ build:
 	go build -o bin/ova-recipe-api ./cmd/ova-recipe-api.go
 
 test-internal:
+	rm -rf ./internal/recipe/*_mock.go
+	mockgen -destination=./internal/recipe/action_mock.go -package=recipe ova-recipe-api/internal/recipe Action
 	go test ./internal/...
--- cmd/ova-recipe-api.go
+++ cmd/ova-recipe-api.go
@@ -4,7 +4,22 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"ova-recipe-api/internal/recipe"
 )
+
+type MockCookAction struct {
+	name string
+}
+
+func (mca *MockCookAction) String() string {
+	return fmt.Sprintf("action name: '%s'", mca.name)
+}
+
+func (mca *MockCookAction) DoAction() error {
+	fmt.Printf("do action '%s'\n", mca.name)
+	return nil
+}
+
 func ReadFileLoop(loopCount uint64, filePath string) {
 	readFileFn := func(filePath string) ([]byte, error) {
 		file, err := os.Open(filePath)
@@ -30,4 +45,19 @@ func ReadFileLoop(loopCount uint64, filePath string) {
 func main() {
 	fmt.Println("Hi, i am ova-recipe-api!")
 	ReadFileLoop(1, "./Makefile")
+	cookieRecipe := recipe.New(
+		1,
+		1,
+		"Oatmeal cookies",
+		"The best oatmeal cookies from the chef",
+		[]recipe.Action{&MockCookAction{"Start to bake"},
+			&MockCookAction{"Continue to bake"},
+			&MockCookAction{"Finish to bake"}},
+	)
+	fmt.Println("Let's cook oatmeal cookies")
+	if err := cookieRecipe.Cook(); err != nil {
+		fmt.Printf("Can not cook %s", cookieRecipe.String())
+	} else {
+		fmt.Printf("%s is ready", cookieRecipe.String())
+	}
 }
