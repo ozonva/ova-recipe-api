47f0530ec711abaaa0a7efb71d21b077eb4d44cd
vasily.popov <vasily.popov@arrival.com>
2021-08-15 18:12:27 +0300

    [task-3] Add SplitRecipeSlice

--- internal/utils/split_slice.go
+++ internal/utils/split_slice.go
@@ -1,5 +1,7 @@
 package utils
 
+import "ova-recipe-api/internal/recipe"
+
 func min(lhs, rhs uint) uint {
 	if lhs < rhs {
 		return lhs
@@ -16,6 +18,20 @@ func SplitIntSlice(srcSlice []int, batchSize uint) [][]int {
 	for startIdx := uint(0); startIdx < srcSliceLen; startIdx += batchSize {
 		endIdx := min(startIdx+batchSize, srcSliceLen)
 		subSlice := append(make([]int, 0, endIdx-startIdx), srcSlice[startIdx:endIdx]...)
+		result = append(result, subSlice)
+	}
+	return result
+}
+
+func SplitRecipeSlice(srcSlice []recipe.Recipe, batchSize uint) [][]recipe.Recipe {
+	if 0 == batchSize {
+		return make([][]recipe.Recipe, 0, 0)
+	}
+	srcSliceLen := uint(len(srcSlice))
+	var result = make([][]recipe.Recipe, 0, (srcSliceLen+batchSize-1)/batchSize)
+	for startIdx := uint(0); startIdx < srcSliceLen; startIdx += batchSize {
+		endIdx := min(startIdx+batchSize, srcSliceLen)
+		subSlice := append(make([]recipe.Recipe, 0, endIdx-startIdx), srcSlice[startIdx:endIdx]...)
 		result = append(result, subSlice)
 	}
 	return result
